6136656519825f2d244c69b751a9a94c23d7fcc3
BrunoAlz <bruno6821@gmail.com>
2022-07-17 20:27:23 -0300

    Implementação da API de Tokens

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Implementa a View genérica para criação dos Usuários"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
