632451bef4bd78cccc99da7bab52d39183a9eb9d
BrunoAlz <bruno6821@gmail.com>
2022-07-17 19:56:44 -0300

    Implementação dos Testes de Listagem das Receitas.
    Ajustes de lint.

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,114 @@
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+
+from recipe.serializers import RecipeSerializer
+
+
+RECIPES_URL = reverse('recipe:recipe-list')
+
+
+# Função helper para os teste das Receitas
+def create_recipe(user, **params):
+    """Cria e retorna uma receita para testes"""
+
+    # 1 - Criando o valores padrões para teste
+    defaults = {
+        'title': 'Titulo da Receita teste ',
+        'time_minutes': 22,
+        'price': Decimal('5.25'),
+        'description': 'Descrição da receita de testes',
+        'link': 'http://teste.com/receita.pdf',
+    }
+    """2 - Atualiza o Dicionário defaults com os dados recebidos
+    nos params, se não vier dados usará os valores padrões"""
+    defaults.update(params)
+
+    # 3 - Cria a receita usando um usuário, e o dicionário defaults
+    recipe = Recipe.objects.create(
+        user=user,
+        **defaults
+    )
+    return recipe
+
+
+class PublicRecipeAPITests(TestCase):
+    """Verifica as requisições de usuários não autenticados."""
+
+    def setUp(self):
+        # 1 - Cria um APIClient que simula as requisições
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        # 2 - Verifica se a autenticação é obrigatória para a requisição.
+        res = self.client.get(RECIPES_URL)
+        # 3 - Verifica o Status da Requisição
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateRecipeApiTests(TestCase):
+    """Verifica as Requisições dos usuários Atutenticados."""
+
+    def setUp(self):
+        # 1 - Cria um APIClient que simula as requisições
+        self.client = APIClient()
+        # 2 - Cria um Usuário django, para ser usado nos testes
+        self.user = get_user_model().objects.create_user(
+            'user@example.com',
+            'testpass123',
+        )
+        # 3 - Força a autenticação do Usuário
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_recipes(self):
+        """Verifica se está recebendo uma lista de Receitas."""
+
+        # 1 - Cria duas Receitas, com os dados padrões
+        create_recipe(user=self.user)
+        create_recipe(user=self.user)
+
+        # 2 - manda um GET na Url de Receitas
+        res = self.client.get(RECIPES_URL)
+        # 3 - Recupera todas as Receitas do BD
+        recipes = Recipe.objects.all().order_by('-id')
+        # 4 - Serializa os dados recuperados do BD
+        serializer = RecipeSerializer(recipes, many=True)
+        # 5 - Testa o Status da requisição
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # 6 - Verifica se os dados da Requisição são iguais do BD
+        self.assertEqual(res.data, serializer.data)
+
+    def test_recipe_list_limited_to_user(self):
+        """Verifica se a listagem devolvida está restrita apenas
+        ao usuário que realizou a solicitação dos dados
+        ."""
+
+        # 1 - Cria um Usuário django, para ser usado nos testes
+        other_user = get_user_model().objects.create_user(
+            'other@example.com',
+            'password123',
+        )
+        # 2 - Cria duas receitas, cada uma por um usuário diferente.
+        create_recipe(user=other_user)
+        create_recipe(user=self.user)
+
+        # 3 - manda um GET na Url de Receitas
+        res = self.client.get(RECIPES_URL)
+        """
+        4 - Recupera as receitas do BD, filtrando o usuário, para que
+        retorne apenas as receitas inseridas pelo usuário da requisição
+        """
+        recipes = Recipe.objects.filter(user=self.user)
+        # 5 - Serializa os dados recuperados do BD
+        serializer = RecipeSerializer(recipes, many=True)
+        # 6 - Testa o Status da requisição
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # 7 - Verifica se os dados da Requisição são iguais do BD
+        self.assertEqual(res.data, serializer.data)
